3f70c3d133cb399e8ea20ee0b61682973dbdb07f
20231069 <20231069@buaa.edu.cn>
2022-11-09 20:45:23 +0800

    zyj-1

---
+++ src/components/LogIn.vue
@@ -0,0 +1,76 @@
+<template>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>网上超市</title>
+  </head>
+  <body >
+  <div class="title">网上超市</div>
+
+  <div class="login-area">
+    账号：<input type="text" placeholder="请输入账号" v-model="usrName">
+    <br>
+    <br>
+    密码：<input type="password" placeholder="请输入密码" v-model="password">
+    <br>
+    <br>
+    <div id="submit-div"><button type="submit" id="submit-btn" @click="doSubmit">登录</button></div>
+    <br>
+    <span><a href="#" id="fgt-psw" @click="test">忘记密码</a></span>
+    <span><a href="./register"  id="free-rgs">免费注册</a></span>
+  </div>
+  </body>
+</template>
+
+<script>
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+  name: "LogIn",
+  data: function() {
+    return{
+      usrName: "",
+      password: ""
+    }
+
+  },
+  methods: {
+    test: function () {
+      alert("????")
+    },
+    handleLogIn: function () {
+      if(this.usrName===localStorage['userName'] && this.password===localStorage['password'])
+      {
+        // this.$router.replace('/Home');//如果输入的名字以及密码正确路由跳转至个人页面
+      }
+    },
+    doSubmit: function () {
+      let params = {
+        usrName: this.usrName,
+        password: this.password
+      };
+      // var url = "http://localhost:8080/"
+      // axios.get(url, {
+      //   params: params
+      // }).then(function (response) {
+      //   console.log(response);
+      // }).catch(function (error) {
+      //   console.log(error)
+      // });
+      // var str = qs.stringify(params);
+      // axios.post(url, str
+      // ).then(function (response) {
+      //   console.log(response);
+      // }).catch(function (error) {
+      //   console.log(error);
+      // });
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @import "login.css";
+</style>
---
+++ src/components/RegisterAccount.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="regAcc" >
+  <head>
+    <meta charset="UTF-8">
+    <title>注册页面</title>
+  </head>
+
+  <body >
+  <p v-text=str> </p>
+  <div class="rgs_layout" id="rgs">
+    <div class="rgs_left">
+      <p>新用户注册</p>
+      <p>USER REGISTER</p>
+    </div>
+
+    <div class="rgs_center">
+      <div class="rgs_form">
+        <form action="#" method="post">
+          <table>
+            <tr>
+              <td class="td_left"><lable for="username">用户名</lable></td>
+              <td class="td_right"><input type="text" name="username" id="username" v-model="usrName"></td>
+            </tr>
+            <tr>
+              <td class="td_left"><lable for="password">密码</lable></td>
+              <td class="td_right"><input type="password" name="password" id="password" v-model="password"></td>
+            </tr>
+            <tr>
+              <td class="td_left"><label for="email">Email</label></td>
+              <td class="td_right"><input type="text" name="email" id="email"></td>
+            </tr>
+            <tr>
+              <td class="td_left"><label for="name">姓名</label></td>
+              <td class="td_right"><input type="text" name="name" id="name"></td>
+            </tr>
+            <tr>
+              <td class="td_left"><label>性别</label> </td>
+              <td class="td_right"><input type="radio" name="gender" value="male">男
+                <input type="radio" name="gender" value="female">女</td>
+            </tr>
+            <tr>
+              <td class="td_left"><lable for="birthday">出生日期</lable></td>
+              <td class="td_right"><input type="date" name="birthday" id="birthday"></td>
+            </tr>
+            <tr>
+              <td class="td_left"><label for="checkcode">验证码</label> </td>
+              <td class="td_right"><input type="text" name="checkcode" id="checkcode">
+                <img id="img_check" src="../assets/checkcode.jpg" width="140"> </td>
+            </tr>
+            <!--                        <tr>-->
+            <!--                            <td colspan="3" id="sub_td"><input type="submit" name="sub_btn" id="sub_btn" value="注册"></td>-->
+            <!--                        </tr>-->
+          </table>
+        </form>
+        <input type="submit" name="sub-btn" id="sub_btn" value="注册" @click="handleRgs">
+      </div>
+    </div>
+
+    <div class="rgs_right">
+      <p>已有帐号？<a href="./login">立刻登录</a></p>
+    </div>
+  </div>
+  </body>
+  </div>
+</template>
+
+
+<script>
+ export default {
+   name: "RegisterAccount",
+   data() {
+     return {
+       usrName:"",
+       password:"",
+       mail:"",
+       tel:""
+     };
+   },
+   methods:{
+     handleRgs: function () {
+       if (localStorage['usrName'] === this.usrName) {
+         alert("用户名已存在");
+       } else if (this.name === '') {
+         alert("用户名不能为空");
+       } else {
+         localStorage.setItem('usrName', this.usrName);
+         localStorage.setItem('password', this.password);
+         alert("注册成功");
+         this.$router.replace('/login');
+       }
+     }
+   }
+ }
+</script>
+
+
+
+
+
+<style scoped>
+
+  @import "register.css";
+</style>
