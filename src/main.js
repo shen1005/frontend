7f27c00205dc3f99e6d865a831627f91d284dabb
陈茂 <20374042@buaa.edu.cn>
2022-11-16 21:00:55 +0800

    add axios in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-
+import axios from "axios";
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.config.productionTip = false
 
 new Vue({
