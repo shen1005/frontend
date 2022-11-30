24ad73a20762171c92e7e288a33f57fde19287b9
WUBO1533085891 <1533085891@qq.com>
2022-11-30 18:54:07 +0800

    change port to 8443

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: 8443,
+    open: true,
+  }
 })
